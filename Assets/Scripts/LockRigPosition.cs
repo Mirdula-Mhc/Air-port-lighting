b18de0107ca8c0969f1792aae88365193e0e8816
Nagendran2005 <mirdula@mhcognition.in>
2026-06-23 02:56:32 +0530

    vr

---
+++ Assets/Scripts/LockRigPosition.cs
@@ -0,0 +1,16 @@
+//using UnityEngine;
+
+//public class LockRigPosition : MonoBehaviour
+//{
+//    private Vector3 startPosition;
+
+//    void Start()
+//    {
+//        startPosition = transform.position;
+//    }
+
+//    void LateUpdate()
+//    {
+//        transform.position = startPosition;
+//    }
+//}
